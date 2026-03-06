20af1adc5d51d7e74fe1867e89f694b90195cd91
Ionatan1we4 <ionatansamuel73@gmail.com>
2026-03-06 13:04:20 +0200

    Update MainActivity.kt

--- app/src/main/java/com/app/assistant/MainActivity.kt
+++ app/src/main/java/com/app/assistant/MainActivity.kt
@@ -4,7 +4,6 @@ import com.google.android.play.core.appupdate.AppUpdateManager
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory
 import com.google.android.play.core.install.model.AppUpdateType
 import com.google.android.play.core.install.model.UpdateAvailability
-import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.play.core.appupdate.AppUpdateOptions
 import android.app.KeyguardManager
 import android.content.Context
